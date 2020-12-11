c8b6cc06c6398b0013f57c74a5a429ed710dd1fe
Sveta <pozdeeva-1997@mail.ru>
2020-12-11 17:07:36 +0500

    FinalyTask1

--- src/aplana/ru/task/FinalyTask1.java
+++ src/aplana/ru/task/FinalyTask1.java
@@ -1,4 +1,27 @@
 package aplana.ru.task;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class FinalyTask1 {
+    private static int y;
+
+    public static void main(String[] args) {
+        // вопрос 1
+        System.out.println("Введите текущий курс рубля к доллару (дробная часть через запятую)");
+        Scanner scan = new Scanner(System.in);
+        double kurs = scan.nextDouble();
+        // вопрос 2
+        System.out.println("Введите количество рублей");
+        double rub = scan.nextDouble();
+
+        // результат
+        System.out.println("Результат:");
+        if (kurs == 0  || rub == 0) {
+            System.out.println("Вы ввели 0");
+        } else {
+            DecimalFormat df = new DecimalFormat("###.##");
+            System.out.println("Итого: " + df.format(rub / kurs) + " долларов");
+        }
+    }
 }
